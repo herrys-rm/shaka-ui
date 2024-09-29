b72c3b389d2c45d79bcaddb10ea0d20f76ee1ecb
heriryan23 <heriryan23@yahoo.co.id>
2024-09-29 13:28:23 +0800

    add template

---
+++ index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Arkitektur - Architecture HTML Template</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600&family=Teko:wght@400;500;600&display=swap" rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<>
+    <!-- Spinner Start -->
+    <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+        <div class="spinner-border position-relative text-primary" style="width: 6rem; height: 6rem;" role="status"></div>
+        <img class="position-absolute top-50 start-50 translate-middle" src="img/icons/icon-1.png" alt="Icon">
+    </div>
+    <!-- Spinner End -->
+
+
+    <!-- Topbar Start -->
+    <?php require_once 'topbar.php' ?>
+    <!-- Topbar End -->
+
+
+    <!-- Navbar Start -->
+    <?php require_once 'topbar.php' ?>
+    <!-- Navbar End -->
+
+
+    <!-- Carousel Start -->
+    <?php require_once 'topbar.php' ?>
+    <!-- Carousel End -->
+
+
+    <!-- Facts Start -->
+    <?php require_once 'features.php' ?>
+    <!-- Facts End -->
+
+
+    <!-- About Start -->
+    <?php require_once 'features.php' ?>
+    <!-- About End -->
+
+
+    <!-- Service Start -->
+    <?php require_once 'service.php' ?>
+    <!-- Service End -->
+
+
+    <!-- Feature Start -->
+    <?php require_once 'why-us.php' ?>
+    <!-- Feature End -->
+
+
+    <!-- Project Start -->
+    <?php require_once 'why-us.php' ?>
+    <!-- Project End -->
+
+
+    <!-- Team Start -->
+    <?php require_once 'team.php' ?>
+    <!-- Team End -->
+
+
+    <!-- Appointment Start -->
+    <?php require_once 'appointment.php' ?>
+    <!-- Appointment End -->
+
+
+    <!-- Testimonial Start -->
+    <?php require_once 'testimonial.php' ?>
+    <!-- Testimonial End -->
+
+
+    <!-- Footer Start -->
+    <?php require_once 'footer.php' ?>
+    <!-- Footer End -->
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/counterup/counterup.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/tempusdominus/js/moment.min.js"></script>
+    <script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
+    <script src="lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+    </body>
+
+</html>
