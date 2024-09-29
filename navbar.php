1ef7804840f2e0d6dc0f7cc563351623d1110a62
heriryan23 <heriryan23@yahoo.co.id>
2024-09-29 14:39:55 +0800

    recolor

--- navbar.php
+++ navbar.php
@@ -1,6 +1,6 @@
-<nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
+<nav class="navbar navbar-expand-lg bg-navbar navbar-light sticky-top py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
     <a href="index.html" class="navbar-brand ms-4 ms-lg-0">
-        <h1 class="text-primary m-0"><img class="me-3" src="img/icons/icon-1.png" alt="Icon">Arkitektur</h1>
+        <h1 class="text-shaka m-0"><img class="me-3" src="img/icons/icon-1.png" alt="Icon">Arkitektur</h1>
     </a>
     <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
         <span class="navbar-toggler-icon"></span>
@@ -23,6 +23,6 @@
             </div>
             <a href="contact.html" class="nav-item nav-link">Contact</a>
         </div>
-        <a href="" class="btn btn-primary py-2 px-4 d-none d-lg-block">Appointment</a>
+        <a href="" class="btn btn-primary py-2 px-4 d-none d-lg-block">Hubungi Kami</a>
     </div>
 </nav>
