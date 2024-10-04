f5ccd7fd7be12746a2f524ccaef7281bf67623be
Sahrilaji98 <sahrilaji98@gmail.com>
2024-10-04 08:16:07 +0800

    store

--- service.php
+++ service.php
@@ -35,7 +35,7 @@
                         <img class="mb-4" src="img/icons/building.png" alt="Icon">
                         <h3 class="mb-3">Desain Sekolah</h3>
                         <p class="mb-4">Disain Sekolah Anda Dengan Disain Yang Menarik</p>
-                        <a class="btn" href="https://www.instagram.com/p/CfQJtoILAu2/"><i class="fa fa-plus text-primary me-3"></i>Lihat Detail</a>
+                        <a class="btn" data-bs-toggle="modal" data-bs-target="#modalSekolah"><i class="fa fa-plus text-primary me-3"></i>Lihat Detail</a>
                     </div>
                 </div>
             </div>
@@ -46,7 +46,7 @@
                         <img class="mb-4" src="img/icons/interior21.png" alt="Icon">
                         <h3 class="mb-3">Desain Interior</h3>
                         <p class="mb-4">Interior Rumah Anda Membosankan, Kami Solusinya</p>
-                        <a class="btn" href="https://www.instagram.com/syakadesign21/"><i class="fa fa-plus text-primary me-3"></i>Lihat Detail</a>
+                        <a class="btn" data-bs-toggle="modal" data-bs-target="#modalInterior"><i class="fa fa-plus text-primary me-3"></i>Lihat Detail</a>
                     </div>
                 </div>
             </div>
@@ -68,7 +68,7 @@
                         <img class="mb-4" src="img/icons/store.png" alt="Icon">
                         <h3 class="mb-3">Desain Store</h3>
                         <p class="mb-4">Disain Ruko Anda Bersama Kami, Pasti Lebih Menarik</p>
-                        <a class="btn" href="https://www.instagram.com/p/DAAUYTdyiAs/"><i class="fa fa-plus text-primary me-3"></i>Lihat Detail</a>
+                        <a class="btn" data-bs-toggle="modal" data-bs-target="#modalStore"><i class="fa fa-plus text-primary me-3"></i>Lihat Detail</a>
                     </div>
                 </div>
             </div>
@@ -175,6 +175,28 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h1 class="modal-title fs-5 text-dark" id="exampleModalLabel">Detail Hotel & Villa</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <a class="btn-ig" href="https://www.instagram.com/p/DASIfugSJLd/?img_index=1">
+                    <p class="fs-5 fw-bold text-center"><i class="fab fa-instagram fs-3 pt-2 me-2"></i>Instagram</p>
+                </a>
+                <a class="btn-facebook" href="https://www.instagram.com/p/DASIfugSJLd/?img_index=1">
+                    <p class="fs-5 fw-bold text-center"><i class="fab fa-facebook fs-3 pt-2 me-2"></i>Facebook</p>
+                </a>
+                <a class="btn-youtube" href="https://www.instagram.com/p/DASIfugSJLd/?img_index=1">
+                    <p class="fs-5 fw-bold text-center"><i class="fab fa-youtube fs-3 pt-2 me-2"></i>Youtube</p>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Modal store -->
+<div class="modal fade" id="modalStore" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5 text-dark" id="exampleModalLabel">Detail Store</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
