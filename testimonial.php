027da52ae6ef588239582274e2757779791bd6df
Sahrilaji98 <sahrilaji98@gmail.com>
2024-10-15 21:16:41 +0800

    good

--- testimonial.php
+++ testimonial.php
@@ -6,28 +6,28 @@
     </div>
     <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
       <div class="carousel-inner">
-        <div class="carousel-item active" data-bs-interval="1500">
+        <div class="carousel-item active" data-bs-interval="10000">
           <div class="row mt-5">
-            <div class="col-lg-4 mb-3">
+            <div class="col col-md-4 mb-3">
               <img src="img/phone-1.png" class="img-fluid w-100 h-auto" alt="">
             </div>
-            <div class="col-lg-4 mb-3">
+            <div class="col col-md-4 mb-3">
               <img src="img/phone-2.png" class="img-fluid w-100 h-auto" alt="">
             </div>
-            <div class="col-lg-4 mb-3">
+            <div class="col col-md-4 mb-3">
               <img src="img/phone-3.png" class="img-fluid w-100 h-auto" alt="">
             </div>
           </div>
         </div>
-        <div class="carousel-item" data-bs-interval="1500">
+        <div class="carousel-item" data-bs-interval="2000">
           <div class="row mt-5">
-            <div class="col-lg-4 mb-3">
+            <div class="col col-md-4 mb-3">
               <img src="img/phone-4.png" class="img-fluid w-100 h-auto" alt="">
             </div>
-            <div class="col-lg-4 mb-3">
+            <div class="col col-md-4 mb-3">
               <img src="img/phone-5.png" class="img-fluid w-100 h-auto" alt="">
             </div>
-            <div class="col-lg-4 mb-3">
+            <div class="col col-md-4 mb-3">
               <img src="img/phone-6.png" class="img-fluid w-100 h-auto" alt="">
             </div>
           </div>
@@ -35,13 +35,11 @@
         <!-- Tambahkan lebih banyak carousel item jika diperlukan -->
       </div>
       <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-        <i class="fas fa-angle-double-left fw-bold fs-1 text-success" aria-hidden="true"></i>
-        <!-- <span class="carousel-control-prev-icon" aria-hidden="true"></span> -->
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Previous</span>
       </button>
       <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-        <i class="fas fa-angle-double-right fw-bold fs-1 text-success" aria-hidden="true"></i>
-        <!-- <span class="carousel-control-next-icon fw-bold"></span> -->
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Next</span>
       </button>
     </div>
